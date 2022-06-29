19c3c40eef4e98fcd19395b5e4e34e4f143b8ab9
Supratouch <supratouchgussii@gmail.com>
2022-06-29 23:42:22 +0700

    initial

---
+++ components/system/Redux/userReducer.js
@@ -0,0 +1,39 @@
+import { UPDATE_USER_INFO } from "./actionTypes";
+
+let initialState = {
+  user: null,
+};
+
+let Reducer = (state = initialState, action) => {
+  // Force something to update
+  switch (action.type) {
+    case UPDATE_USER_INFO:
+      let status = false;
+      if (action.user != null) {
+        status = true;
+      } else {
+        status = false;
+      }
+      let user = action.user;
+      let isLogin = status;
+      let fitness = state.fitness + 1;
+      return {
+        ...state,
+        user,
+        isLogin,
+        fitness,
+      };
+      break;
+      return {
+        ...state,
+        Gameplay: {
+          ...state.Gameplay,
+          details: {},
+        },
+      };
+    default:
+      return state;
+  }
+};
+
+export default Reducer;
