7dc5bdc80fe7846d72b9ed0a8d1f675d1ab86395
Supratouch <supratouchgussii@gmail.com>
2022-06-30 01:59:13 +0700

    merge

--- components/system/jotai/store.ts
+++
@@ -1,9 +0,0 @@
-import { atom } from "jotai";
-import { User } from "@firebase/auth/dist/auth-public";
-import { Profile } from "./type";
-
-export const firebaseReady = atom<boolean>(false);
-export const firebaseUserAtom = atom<User | null>(null);
-export const firebaseToken = atom<string | null>(null);
-
-export const profileInfoAtom = atom<Profile | null>(null);
